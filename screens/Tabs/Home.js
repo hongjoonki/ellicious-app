06033136b60fef158a70c7d7dd6cd4aedefd34ca
hongjoonki <ghdjk2216@naver.com>
2019-11-04 21:55:22 +0900

    #search

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -4,35 +4,16 @@ import styled from "styled-components";
 import { gql } from "apollo-boost";
 import Loader from "../../components/Loader";
 import { useQuery } from "react-apollo-hooks";
+import Post from "../../components/Post";
+import { POST_FRAGMENT } from "../../fragments";
 
 const FEED_QUERY = gql`
   {
     seeFeed {
-      id
-      location
-      caption
-      user {
-        id
-        avatar
-        username
-      }
-      files {
-        id
-        url
-      }
-      likeCount
-      isLiked
-      comments {
-        id
-        text
-        user {
-          id
-          username
-        }
-      }
-      createdAt
+      ...PostParts
     }
   }
+  ${POST_FRAGMENT}
 `;
 
 const View = styled.View`
